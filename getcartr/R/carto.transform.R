73c331188fb34f1379f821325eb120b0d15b9260
Chris Brunsdon <christopher.brunsdon@nuim.ie>
2014-11-15 16:38:07 +0000

    Updated defaults on carto.transform

--- getcartr/R/carto.transform.R
+++ getcartr/R/carto.transform.R
@@ -5,10 +5,10 @@
 #'
 #' @param spdf A \code{SpatialPolygonsDataFrame}
 #' @param v The variable providing the values - typically population counts
-#' @param extend The extent beyond the limits of the \code{SpatialPolygonsDataFrame} to estimate the warping as a proportion of span
-#' @param res The resolution of the grid for the warping
+#' @param extend The extent beyond the limits of the \code{SpatialPolygonsDataFrame} to estimate the warping as a proportion of span - default 0.05
+#' @param res The resolution of the grid for the warping - default 128
 #' @param index Index of data item to be used for cartogram - only used if \code{v} is missing
-#' @param thresh Lowest density value allowed - will replace pixels with \code{max(value,thresh*mean(value))} - 0.1 works well
+#' @param thresh Lowest density value allowed - will replace pixels with \code{max(value,thresh*mean(value))} - 0.1 works well 
 #' @return a function taking a \code{Spatial*} or \code{Spatial*DataFrame} object,  returning a warped version of the object
 #' 
 #' @references A Diffusion-based method for producing density equalizing maps, Michael T. Gastner and M. E. J. Newman, Proc. Natl. Acad. Sci. USA 101, 7499-7504 (2004)
@@ -31,7 +31,7 @@
 #' 
 #' 
 #' 
-carto.transform <- function(spdf,v,extend=0.05,res=128,index=1,thresh=0) {
+carto.transform <- function(spdf,v,extend=0.05,res=128,index=1,thresh=0.1) {
   poly <- quick.carto(spdf,v,extend,res,index,thresh)
   result <- function(obj) {
     if (grepl("SpatialPolygons",class(obj))) return(warp.polys(obj,poly))
